fb27cde69b3ccd6322ca8000dcf58a88c3180ea8
Calum Leslie <calumleslie@gmail.com>
2015-11-27 21:43:10 +0000

    Run everything through rustfmt.
    Added a rustfmt_skip in one place because it got weirded up a bit.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod vector4;
 use simd::f32x4;
 
 fn main() {
-	let x = f32x4::new(1.0, 2.0, 3.0, 4.0);
+    let x = f32x4::new(1.0, 2.0, 3.0, 4.0);
 
-    println!("Hello, world! {:?}", x*x);
+    println!("Hello, world! {:?}", x * x);
 }
